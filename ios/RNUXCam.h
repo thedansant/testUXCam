1f5469f0040a8f5127a0daaf4f6a6ff84f018600
Maxime Bompoil <maxime.bompoil@gmail.com>
2018-10-29 18:07:32 +0100

    test

---
+++ ios/RNUXCam.h
@@ -0,0 +1,14 @@
+//
+//  RNUXCam.h
+//  RNUXCam
+//
+//  Created by Mark Miyashita on 10/22/16.
+//  Copyright © 2016 Mark Miyashita. All rights reserved.
+//
+
+
+#import <React/RCTBridgeModule.h>
+
+
+@interface RNUXCam : NSObject <RCTBridgeModule>
+@end
